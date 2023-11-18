15d6cd1a9d29a504bbd24fc7b59cbd2b00c29ab3
Ruslan Rozbeiko <rrozb@gmail.com>
2023-11-18 17:19:37 +0100

    ENH: wip mvp research

--- trading_env_gym_custom/simple_pipe.py
+++ trading_env_gym_custom/simple_pipe.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import torch
 from gymnasium import register
+from sb3_contrib import RecurrentPPO
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import EvalCallback
 from stable_baselines3.common.monitor import Monitor
@@ -28,6 +29,22 @@ register(
     # Change 'path_to_your_module' to the module path of your environment
 )
 
+
+def get_warmup_data(test_df, current_period, warmup_length=1):
+    """
+    Fetches warmup data for LSTM.
+
+    Parameters:
+    test_df (DataFrame): The dataframe containing test data.
+    current_period (Period): The current period for which the model is being evaluated.
+    warmup_length (int): Number of periods to include in the warmup.
+
+    Returns:
+    DataFrame: Warmup data for the LSTM model.
+    """
+    warmup_periods = [current_period - i for i in range(1, warmup_length + 1)]
+    warmup_data = test_df[test_df['period'].isin(warmup_periods)]
+    return warmup_data
 
 def linear_schedule(initial_value, final_value):
     def schedule(progress_remaining):
@@ -109,6 +126,7 @@ def create_envs(train_df, eval_df, test_df):
                                 trading_fees=0.01 / 100,
                                 borrow_interest_rate=0.0003 / 100,
                                 reward_function=simple_reward,
+                                windows=24 * 7,
 
                                 ))
     train_env = Monitor(gym.make("TradingEnv",
@@ -117,7 +135,9 @@ def create_envs(train_df, eval_df, test_df):
                                  positions=[-1, 0, 1],
                                  trading_fees=0,
                                  borrow_interest_rate=0,
-                                 reward_function=simple_reward
+                                 reward_function=simple_reward,
+                                 windows=24 * 7,
+
                                  ))
 
     test_env = Monitor(gym.make("TradingEnv",
@@ -126,7 +146,8 @@ def create_envs(train_df, eval_df, test_df):
                                 positions=[-1, 0, 1],
                                 trading_fees=0.01 / 100,  # 0.01% per stock buy / sell (Binance fees)
                                 borrow_interest_rate=0.0003 / 100,  # 0.0003% per timestep (one timestep = 1h here),
-                                reward_function=simple_reward
+                                reward_function=simple_reward,
+                                windows=24 * 7,
                                 ))
 
     return train_env, eval_env, test_env
@@ -179,24 +200,40 @@ def validate_and_roll_train(model_class, data_dir, save_dir, period='W'):
     # Load the best model
     # unique_save_dir = os.path.join(save_dir, 'best_model')
     best_model = load_best_model(model_class, save_dir)
-    total_reward = 0
-    for p in periods:
-        # Create a subset of test data for the current period
-        period_df = test_df[test_df['period'] == p]
+    _, eval_env, test_env = create_envs(train_df, eval_df, test_df)
 
-        # Create environment for the current period
+    best_model.set_env(eval_env)
+    eval_model = evaluate_model(best_model, eval_env, save_dir)
+    print(f"Eval model: {eval_model}")
+    best_model.learn(total_timesteps=len(eval_df) * 2)
+
+    best_model.set_env(test_env)
+    test_model = evaluate_model(best_model, test_env, save_dir)
+    print(f"TEST not training model: {test_model}")
+
+    total_reward = []
+
+    for p in periods[1:]:
+        print(f"Training on period {p}")
+        # Create a subset of test data for the current period
+        period_df = test_df[test_df['period'].isin([p - 1, p])]
+
+        # Create environment for the combined data
         _, _, test_env_period = create_envs(train_df, eval_df, period_df)
 
         # Set the environment to the current period
         best_model.set_env(test_env_period)
-        best_model.learning_rate= 2.0e-3
 
         # Evaluate model on the current period without training
-        total_reward += evaluate_model(best_model, test_env_period, save_dir)
+        total_reward.append(evaluate_model(best_model, test_env_period, save_dir))
+
+        # Reset the environment state if necessary before training
+        test_env_period.reset()
 
         # Train model on the current period
-        best_model.learn(total_timesteps=len(period_df) * 5)
-    print(f"Total reward: {total_reward}")
+        best_model.learn(total_timesteps=len(period_df) * 2)
+
+    print(f"Total reward: {sum(total_reward)}")
 
 
 def train_and_save_pipeline(data_dir, save_dir):
@@ -225,7 +262,7 @@ def train_and_save_pipeline(data_dir, save_dir):
                                  eval_freq=50_000,
                                  n_eval_episodes=2,
                                  deterministic=True, render=False)
-    total_timesteps = 3_000_000
+    total_timesteps = 300_000
     learning_rate_schedule = linear_schedule(3e-4, 2.5e-5)
     clip_range_schedule = linear_schedule(0.3, 0.1)
     # ent_coef_schedule = linear_schedule(0.1, 0.01)
@@ -243,10 +280,11 @@ def train_and_save_pipeline(data_dir, save_dir):
         'max_grad_norm': 0.5,
     }
     # 2. Training
-    model = model_class('MlpPolicy', train_env, verbose=1, **hyperparams, tensorboard_log=base_log_path)
+    # model = model_class('MlpPolicy', train_env, verbose=1, **hyperparams, tensorboard_log=base_log_path)
+    model = RecurrentPPO("MlpLstmPolicy", train_env, **hyperparams, verbose=1, tensorboard_log=base_log_path)
     model.learn(total_timesteps=total_timesteps, callback=eval_callback)
 
-    validate_and_roll_train(model_class, data_dir, unique_save_dir)
+    # validate_and_roll_train(model_class, data_dir, unique_save_dir)
 
     # best_model = load_best_model(model_class, unique_save_dir)
     # best_model.set_env(eval_env)
@@ -366,9 +404,9 @@ set_seeds(42)
 # process_data("/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/data/bitfinex2-BTCUSD-5m.pkl",
 #              date_indexes=[pd.to_datetime("2023-08-01"), pd.to_datetime("2023-09-01")], granularity='5m')
 
-train_and_save_pipeline('/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/data/bitfinex2-BTCUSD-5m',
-'/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/training_results')
+# train_and_save_pipeline('/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/data/bitfinex2-BTCUSD-1h_2015-2023',
+# '/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/training_results')
 
-# validate_and_roll_train(PPO,
-#                         '/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/data/bitfinex2-BTCUSD-1h_2015-2023',
-#                         '/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/training_results/run_20231022_075452')
+validate_and_roll_train(RecurrentPPO,
+                        '/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/data/bitfinex2-BTCUSD-1h_2015-2023',
+                        '/home/rr/Documents/Coding/Work/crypto/reinforcement-learning-for-trading/trading_env_gym_custom/training_results/run_20231118_101636')
